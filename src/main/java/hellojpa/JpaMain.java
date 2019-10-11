f7f1e85ed02ee7c768ab8b816fccf4196f7c65cc
JohnMark <practice1356@gmail.com>
2019-10-11 13:38:35 +0900

    bidirection relation set master

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -20,9 +20,8 @@ public class JpaMain {
 
             Member member = new Member();
             member.setUsername("member1");
+            member.setTeam(team);
             em.persist(member);
-
-            team.getMembers().add(member);
 
             em.flush();
             em.clear();
